7f460d4486f0b6bc2196948f02ce7d07bb36b7e8
Claudia Maia <maiaandradec@gmail.com>
2025-06-30 10:30:18 -0300

    username added on auth routes

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -5,7 +5,6 @@ from models.user import User
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/api")
 
-
 @auth_bp.route("/register", methods=["POST"])
 def register():
     """
@@ -25,6 +24,7 @@ def register():
           required:
             - email
             - password
+            - username
           properties:
             email:
               type: string
@@ -32,29 +32,35 @@ def register():
             password:
               type: string
               example: 123456
+            username:
+              type: string
+              example: joaosilva
     responses:
       201:
         description: Usuário registrado com sucesso
       400:
-        description: Email já registrado ou dados inválidos
+        description: Email ou username já registrado ou dados inválidos
     """
     data = request.get_json()
     email = data.get("email")
     password = str(data.get("password"))
+    username = data.get("username")
 
-    if not email or not password:
-        return {"message": "Email e senha são obrigatórios"}, 400
+    if not email or not password or not username:
+        return {"message": "Email, senha e nome de usuário são obrigatórios"}, 400
 
     if User.query.filter_by(email=email).first():
         return {"message": "Email já registrado"}, 400
 
-    user = User(email=email)
+    if User.query.filter_by(username=username).first():
+        return {"message": "Nome de usuário já registrado"}, 400
+
+    user = User(email=email, username=username)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
 
     return {"message": "Usuário registrado com sucesso"}, 201
-
 
 
 @auth_bp.route("/login", methods=["POST"])
@@ -101,4 +107,7 @@ def login():
         return {"message": "Email ou senha inválidos"}, 401
 
     access_token = create_access_token(identity=str(user.id))
-    return {"access_token": access_token}, 200
+    return {
+        "access_token": access_token,
+        "username": user.username
+    }, 200
